761f43e3da982f1c8f66a5e5b044c837f11f910d
chaikritt <techaploog@gmail.com>
2023-07-31 14:59:51 +0700

    Implement EMA class: modified: myfin/indicators/ema.py
    changed:
    - implement function __init__()
    - implement function apply()
    - modify function __str__() to return it's desc property

--- myfin/indicators/ema.py
+++ myfin/indicators/ema.py
@@ -1,16 +1,17 @@
+import pandas as pd
 from .indicator import Indicator
 
-datatype = {
-    "data":list
-}
-
 class EMA(Indicator):
-    def __init__(self,period):
+    def __init__(self,period:int,column:str='Close'):
         self.period = period
+        self.column = column
+        self.desc = "EMA_{period}".format(period=self.period)
 
-    def apply(self,data):
-        # TODO: implement this function
-        pass
+    def apply(self,serie:pd.Series | pd.DataFrame):
+        data = serie[[self.column]].copy()
+        data = data.ewm(span=self.period).mean()
+        data.columns = [self.desc]
+        return data
 
     def __str__(self):
-        return "EMA_{period}".format(period=self.period)
+        return self.desc
