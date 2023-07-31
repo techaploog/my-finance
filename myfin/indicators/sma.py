aed791978c02e831be5e46b22c617b1176d9eb03
chaikritt <techaploog@gmail.com>
2023-07-31 15:04:06 +0700

    modify column property and how to handle them with pandas

--- myfin/indicators/sma.py
+++ myfin/indicators/sma.py
@@ -2,13 +2,13 @@ import pandas as pd
 from .indicator import Indicator
 
 class SMA(Indicator):
-    def __init__(self,period,columns=["Close"]):
+    def __init__(self,period:int,column:str ="Close"):
         self.period = period
-        self.columns = columns
+        self.column = column
         self.desc = "SMA_{period}".format(period=self.period)
 
     def apply(self,serie:pd.Series | pd.DataFrame):
-        data = serie[self.columns].copy()
+        data = serie[[self.column]].copy()
         data = data.rolling(self.period).mean()
         data.columns = [self.desc]
         return data
